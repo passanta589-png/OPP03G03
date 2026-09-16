3675fa61b61ac5c2310729bef71837066ec53499
passant ahmed <passantahmed992006@outlook.com>
2026-09-17 00:44:14 +0300

    Q04

---
+++ OPP03G03/StandardShipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP03G03
+{
+    internal class StandardShipment : Shipment
+    {
+        public StandardShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+        public override void PrintShipment()
+        {
+            Console.WriteLine("Standard Shipment\n");
+            base.PrintShipment();
+        }
+    }
+}
