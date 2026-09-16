44f65787f2385807139d1ef8f345132454ff0a8d
passant ahmed <passantahmed992006@outlook.com>
2026-09-17 00:36:42 +0300

    q01

--- OPP03G03/Program.cs
+++ OPP03G03/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace OPP03G03
+﻿using System.Timers;
+
+namespace OPP03G03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region 01
+            //a) Overloading بيكون في نفس الكلاس بنفس الاسم بس بباراميترز مختلفة. أما Overriding فبيكون بين كلاس أساسي وكلاس فرعي لنفس الميثود باستخدام override
+            //b)Static binding بيحصل وقت الـ Compile-time(في الـ Overloading).أما Dynamic binding بيحصل وقت الـ Run-time بناءً على نوع الكائن الفعلي في الـ Overriding
+            #endregion
+            #region 02
 
+            #endregion
         }
     }
 }
---
+++ OPP03G03/Shipment.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP03G03
+{
+    internal class Shipment
+    {
+        private string _Trackingcode;
+        private string _Description;
+        private double _Weight;
+        private decimal _DeliveryFee;
+
+        public string TrackingCode
+        {
+            get { return _Trackingcode; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                { _Trackingcode = value; }
+            }
+        }
+        public string Description
+
+        {
+            get { return _Description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                { Description = value; }
+            }
+
+        }
+        public decimal Weight
+        {
+            get { return Weight; }
+            set
+
+            {
+                if (value > 0)
+                { Weight = value; }
+            }
+        }
+        public decimal DeliveryFee
+        {
+            get { return DeliveryFee; }
+
+            private set
+            {
+                if (value >= 0)
+                { DeliveryFee = value; }
+            }
+
+        }
+        internal DeliveryAddress Destination { get; set; }
+
+        public virtual decimal estimatedCost
+        {
+            get
+            {
+                return Weight * 5 + DeliveryFee;
+            }
+        }
+
+        internal Shipment(string trackingcode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            TrackingCode = string.IsNullOrWhiteSpace(trackingcode) ? "N/A" : trackingcode;
+            Description = string.IsNullOrWhiteSpace(description) ? "N/A" : description;
+            Weight = weight > 0 ? weight : 1;
+            DeliveryFee = deliveryFee >= 0 ? deliveryFee : 50;
+            this.Destination = destination;
+        }
+        public void updateWeight(decimal newWeight)
+        {
+            if (newWeight > 0)
+            {
+                Weight = newWeight;
+            }
+
+        }
+        public void updatWeightAndDeliveryFee(decimal newWeight, double extrapackingweight)
+        {
+            if (newWeight > 0 && extrapackingweight >= 0)
+            {
+                Weight = newWeight + (decimal)extrapackingweight;
+            }
+        }
+        public virtual void PrintShipment()
+        {
+            Console.WriteLine($"Tracking Code  : {TrackingCode}");
+            Console.WriteLine($"Description    : {Description}");
+            Console.WriteLine($"Weight         : {Weight} kg");
+            Console.WriteLine($"Delivery Fee   : {DeliveryFee} EGP");
+            Console.WriteLine($"Destination    : {Destination.GetFullAddress()}");
+            Console.WriteLine($"Estimated Cost : {estimatedCost} EGP");
+        }
+
+    }
+}
