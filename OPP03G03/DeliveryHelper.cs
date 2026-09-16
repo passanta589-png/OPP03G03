bc01037cdec0299bd4077f1f455888082de30432
passant ahmed <passantahmed992006@outlook.com>
2026-09-17 01:00:21 +0300

    Q07

---
+++ OPP03G03/DeliveryHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP03G03
+{
+    internal class DeliveryHelper
+    {
+        public static void PrintShipmentDetails(Shipment shipment)
+        {
+            shipment.PrintShipment();
+        }
+    }
+}
