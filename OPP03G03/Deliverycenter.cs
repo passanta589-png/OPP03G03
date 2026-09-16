46ec04e25f107e3f8fa4520a8f2cae6dbfe3932c
passant ahmed <passantahmed992006@outlook.com>
2026-09-17 00:59:09 +0300

    Q06

---
+++ OPP03G03/Deliverycenter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP03G03
+{
+    internal class Deliverycenter
+    {
+        private Shipment[] _shipments;
+        private int _count;
+        private int _Index;
+        public Driver driver { get; set; }
+
+        public Deliverycenter()
+        {
+            _shipments = new Shipment[10];
+            _count = 0;
+        }
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+                return _shipments[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+                _shipments[index] = value;
+            }
+
+        }
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    if (_shipments[i].TrackingCode == trackingCode)
+                    {
+                        return _shipments[i];
+
+
+                    }
+                }
+                throw new KeyNotFoundException("Shipment with the specified tracking code not found.");
+            }
+        }
+        public bool AddShipment(Shipment shipment)
+        {
+            if (_count >= _shipments.Length)
+            {
+                Array.Resize(ref _shipments, _shipments.Length * 2);
+
+                _shipments[_count] = shipment;
+                _count++;
+                return true;
+            }
+            return false;
+        }
+        public void PrintAllShipments()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                _shipments[i].PrintShipment();
+                Console.WriteLine();
+            }
+        }
+    }
+}
